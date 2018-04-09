9dcbf0071f0b15a5e34a731e5b5e99008403360d
Jinfeng Li <lijinf8@gmail.com>
2018-04-09 16:57:41 +0800

    add script to id fvecs

---
+++ script/idfvecs.py
@@ -0,0 +1,23 @@
+#fvecs_file = "../gqr/data/audio/audio_base.fvecs" 
+#output_file = "./audio_base.idfvecs"
+
+dataset = "audio_base"
+fvecs_file = "../gqr/data/audio/" + dataset + ".fvecs"
+output_file = dataset + ".idfvecs"
+
+import struct
+fout = open(output_file, "wb")
+with open(fvecs_file, "rb") as fin:
+    itemId = 0
+    while True:
+        dimension = fin.read(4)
+        if dimension == '':
+            break
+        int_dimension = struct.unpack('I', dimension)[0]
+        vecs = fin.read(int_dimension * 4)
+        fout.write(struct.pack('I' ,itemId))
+        fout.write(dimension)
+        fout.write(vecs)
+        itemId += 1;
+
+fout.close()
