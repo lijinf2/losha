22e9b02b496fe99d0615dde6a46056c416d581bf
Jinfeng Li <lijinf8@gmail.com>
2018-04-11 21:58:14 +0800

    add cal recall

---
+++ script/evaluate.sh
@@ -0,0 +1,15 @@
+# only supports lines of triplets in the format of (srcId, dstId, dist)
+
+# mkdir tmp
+# rm -rf tmp/output.txt
+# hadoop dfs -cat /losha/output/* > tmp/output.txt
+
+lshbox_file="../gqr/data/audio/audio_groundtruth.lshbox"
+triplets_file="tmp/output.txt"
+
+binary_file="tmp/evaluate_triplets"
+if [ ! -f $binary_file ]; then
+    g++ --std=c++11 -I ../gqr -I ../gqr/include  -g src/evaluate_triplets.cpp -o $binary_file 2>&1 | tee log.txt
+fi
+
+gdb --args tmp/evaluate_triplets $lshbox_file $triplets_file
---
+++ script/src/evaluate_triplets.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <utility>
+#include <string>
+#include "../../gqr/apps/opq_evaluate.cpp"
+using namespace std;
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        cout << "Usage: evalaute_triplets lshbox_file triplets_file" << endl;
+        return -1;
+    }
+
+    const char* lshbox_file = argv[1];
+    const char* triplets_file = argv[2];
+
+    Bencher lshboxBench(lshbox_file);
+    unsigned numQueries = lshboxBench.size();
+    vector<vector<pair<unsigned, float>>> results(numQueries, vector<pair<unsigned, float>>());
+
+    ifstream fin(triplets_file);
+
+    if (!fin) {
+        cout << "cannot open file " << triplets_file << endl;
+        return -1;
+    }
+
+    string line;
+    int queryId;
+    int itemId;
+    float distance;
+    while(getline(fin, line)) {
+        istringstream iss(line);
+        iss >> queryId >> itemId >> distance;
+        results[queryId].push_back(make_pair(itemId, distance));
+    }
+
+    for (auto& vec : results) {
+        sort(vec.begin(), vec.end(),
+            [](const pair<unsigned, float>& a, const pair<unsigned, float>& b) {
+            return a.second < b.second;
+        });
+    }
+    fin.close();
+
+    cout << "avg recall:" << cal_avg_recall(lshboxBench, results, true) << endl;
+    cout << "avg error: " << cal_avg_error(lshboxBench, results, true) << endl;
+
+}
