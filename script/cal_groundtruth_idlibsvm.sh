47f25475ee4d10dd7498961df61069df64166a1c
Jinfeng Li <lijinf8@gmail.com>
2018-04-20 11:58:42 +0800

    improve

--- script/cal_groundtruth_idlibsvm.sh
+++ script/cal_groundtruth_idlibsvm.sh
@@ -15,17 +15,21 @@ do
     base_file="../data/idlibsvm/${dataset}/${dataset}_base.idlibsvm"
     query_file="../data/idlibsvm/${dataset}/${dataset}_query.idlibsvm"
 
-    # output groundtruth files, support both ivecs and lshbox formats 
-    lshbox_bench_file="../data/idlibsvm/${dataset}/${dataset}_groundtruth.lshbox"
-    if [ $metric != "euclidean" ]
-    then
-        lshbox_bench_file="../data/idlibsvm/${dataset}/${dataset}_${metric}_groundtruth.lshbox"
+    # output groundtruth files, support both lshbox formats 
+    lshbox_bench_file="../data/idlibsvm/${dataset}"
+
+    if [ $metric == "euclidean" ]
+    then 
+        lshbox_bench_file="${lshbox_bench_file}/${dataset}_groundtruth.lshbox"
+    else
+        lshbox_bench_file="${lshbox_bench_file}/${dataset}_${metric}_groundtruth.lshbox"
     fi
 
     if [[ $queryType == "radius:"* ]]
     then
-        lshbox_bench_file="${lshbox_bench_file}-radius"
+        lshbox_bench_file="${lshbox_bench_file}_radius"
     fi
+
 
     tmp/cal_groundtruth_idlibsvm $base_file $query_file $queryType $lshbox_bench_file $metric $numThreads
 done
