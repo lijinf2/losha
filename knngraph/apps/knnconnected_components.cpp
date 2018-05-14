a2c36170d75c49f922b8a798ba6c5253de6921eb
Jinfeng Li <lijinf8@gmail.com>
2018-05-14 19:51:46 +0800

    add knngraph cc computation

---
+++ knngraph/apps/knnconnected_components.cpp
@@ -0,0 +1,114 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include "boost/tokenizer.hpp"
+
+#include "core/engine.hpp"
+#include "io/input/inputformat_store.hpp"
+
+#include "knnconnected_components.h"
+using std::vector;
+using std::string;
+using namespace husky::losha;
+
+class CCVertex {
+public:
+    using KeyT = int;
+    KeyT _vid;
+    vector<int> _nbs;
+    int _label; 
+    explicit CCVertex(int id) : _vid(id), _label(id) {
+        _vid = id;
+    }
+    const KeyT id() const {return _vid;};
+
+};
+
+void knnconnected_components() {
+    string graphPath = husky::Context::get_param("knngraph_path");
+    auto& lineInputformat =
+        husky::io::InputFormatStore::create_line_inputformat();
+    lineInputformat.set_input(graphPath);
+
+    auto & vertex_list =
+        husky::ObjListStore::create_objlist<CCVertex>();
+
+    auto& channel = 
+        husky::ChannelStore::create_push_channel<int>(lineInputformat, vertex_list);
+
+    auto parser = [&channel](boost::string_ref& line) {
+        boost::char_separator<char> sep(" \t");
+        boost::tokenizer<boost::char_separator<char>> tok(line, sep);
+        int size = 0;
+        for (auto& w : tok) {
+            size++;
+        }
+        if (size <= 2) return;
+
+        int queryId;
+        int itemId;
+        float distance;
+        vector<int> knn;
+
+        auto it = tok.begin();
+        queryId = std::stoi(*it++);
+
+        while(it != tok.end()) {
+            itemId = std::stoi(*it++);
+            distance = std::stoi(*it++);
+            channel.push(itemId, queryId);
+            channel.push(queryId, itemId);
+        }
+    };
+    husky::load(lineInputformat, parser);
+
+    husky::list_execute(
+        vertex_list,
+        {&channel},
+        {},
+        [&channel](CCVertex& vertex){
+            vertex._nbs = channel.get(vertex);
+            // auto msgs = channel.get(vertex);
+            // assert(msgs.size() == 1);
+            // vertex._nbs = msgs[0];
+
+        
+        });
+
+    int numNodes = count(vertex_list);
+    auto getLabelRef = 
+        [](CCVertex& vertex) -> int&{
+            return vertex._label;
+        };
+
+    auto getNeighborsRef =  
+        [](CCVertex& vertex) -> const vector<int>& {
+            return vertex._nbs;
+        };
+
+    KNNConnectedComponents::compute<CCVertex>(
+        vertex_list,
+        getLabelRef,
+        getNeighborsRef);
+
+    auto blockToSize = 
+        KNNConnectedComponents::getClusteringDesc<CCVertex>(
+           vertex_list, 
+           [](const CCVertex& vertex){ return vertex._label;});
+
+    KNNConnectedComponents::reportClustering(blockToSize);
+}
+
+int main(int argc, char ** argv) {
+    std::cout << "start to run program knnconnected_components" << std::endl;
+    std::vector<std::string> args;
+    args.push_back("hdfs_namenode");
+    args.push_back("hdfs_namenode_port");
+    args.push_back("knngraph_path"); 
+    args.push_back("output_path"); 
+    if (husky::init_with_args(argc, argv, args)) {
+        husky::run_job(knnconnected_components);
+        return 0;
+    }
+    return 1;
+}
--- knngraph/apps/knnconnected_components.h
+++ knngraph/apps/knnconnected_components.h
@@ -1,108 +1,91 @@
-#include "core/engine.hpp"
 #include <functional>
 #include <vector>
 #include <utility>
+
+#include "core/engine.hpp"
+#include "lib/aggregator_factory.hpp"
+
+#include "knnagg.h"
 using std::vector;
 using std::pair;
 
 namespace husky {
 namespace losha {
 
-class CCVertex {
-public:
-    int _vid;
-    vector<int> _nbs;
-    int _label; 
-    explicit AdjRecord(int id) : _vid(id), _label(id) {
-        _vid = id;
-    }
-
-};
-
 class KNNConnectedComponents {
 public:
     template<typename ObjT>
-    static void run(
+    static void compute(
         husky::ObjList<ObjT>& obj_list,
-        std::function<int(const ObjT&)> getVertexId, 
-        std::function<vector<int>(const ObjT&)> getNeighbors);
+        std::function<int& (ObjT&)> getLabelRef, 
+        std::function<const vector<int>& (ObjT&)> getNeighborsRef);
 
     template<typename ObjT>
-    static vector<pair<int, int>> KNNConnectedComponents::getClusteringDesc(
+    static vector<pair<int, int>> getClusteringDesc(
         husky::ObjList<ObjT>& obj_list,
         std::function<int(const ObjT&)> getLabel);
 
-    template<typename ObjT>
-    static void KNNConnectedComponents::reportClustering(
-        husky::ObjList<ObjT>& vertex_list);
+    static void reportClustering(
+        const vector<pair<int, int>>& blockToSize);
 };
 
 template<typename ObjT>
-void KNNConnectedComponents::run(
+void KNNConnectedComponents::compute(
     husky::ObjList<ObjT>& obj_list,
-    std::function<int(const ObjT&)> getVertexId, 
-    std::function<vector<int>(const ObjT&)> getNeighbors) {
+    std::function<int& (ObjT&)> getLabelRef, 
+    std::function<const vector<int>& (ObjT&)> getNeighborsRef) {
 
-    auto vertex_list =
-        husky::ObjListStore::create_objlist<CCVertex>();
-    
-    thread_local auto& msgCh =
-            husky::ChannelStore::create_push_channel<vector<int>>(obj_list, vertex_list);
+        int numNodes = count(obj_list);
+    thread_local auto& minCombineCh =
+            husky::ChannelStore::create_push_combined_channel<int, husky::MinCombiner<int>>(obj_list, obj_list);
 
     husky::list_execute(
         obj_list,
         {},
-        {&msgCh},
-        [&msgCh, &getVertexId, &getNeighbors](ObjT& obj){
-        msgCh.push(getNeighbors(obj), getVertexId(obj);
-    });
-
-    thread_local auto& minCombineCh =
-            husky::ChannelStore::create_push_combined_channel<int, husky::MinCombiner<int>>(vertex_list, vertex_list);
-    husky::list_execute(
-        vertex_list,
-        {&msgCh},
         {&minCombineCh},
-        [&msgCh, &minCombineCh](CCVertex& vertex){
-        vertex._nbs = msgCh.get(vertex);
-        for (auto nb : vertex._nbs) {
-            if (nb > vertex._label) {
-                minCombineCh.push(vertex._label, nb);
+        [&minCombineCh, &getLabelRef, &getNeighborsRef](ObjT& obj){
+        getLabelRef(obj) = obj.id();
+        for (const int& nb : getNeighborsRef(obj)) {
+            if (nb > getLabelRef(obj)) {
+                minCombineCh.push(getLabelRef(obj), nb);
             }
         }
     });
 
     husky::lib::Aggregator<int> not_finished(0, [](int& a, const int& b) { a += b; });
     not_finished.to_reset_each_iter();
-    not_finished.update(1);
     auto& agg_ch = husky::lib::AggregatorFactory::get_channel();
     husky::lib::AggregatorFactory::sync();
     int iteration = 0;
 
-
-    while(not_finished.get_value()) {
-        // calculate block with minimum items
+    while(true) {
         husky::list_execute(
-            vertex_list,
+            obj_list,
             {&minCombineCh},
             {&minCombineCh, &agg_ch},
-            [&minCombineCh, &not_finished](CCVertex& vertex){
-            if (minCombineCh.hash_msg(vertex)) {
-                int msg = minCombineCh.get(vertex); 
-                if (vertex._label > msg) {
-                    vertex._label = msg;
-                    if (nb > vertex._label) {
-                        minCombineCh.push(vertex._label, nb);
-                    }
+            [&minCombineCh, &not_finished, &getLabelRef, &getNeighborsRef](ObjT& obj){
+            if (minCombineCh.has_msgs(obj)) {
+                int msg = minCombineCh.get(obj); 
+                if (getLabelRef(obj) > msg) {
                     not_finished.update(1);
+                    getLabelRef(obj) = msg;
+                    for (auto nb : getNeighborsRef(obj)) {
+                        if (nb > msg) {
+                            minCombineCh.push(msg, nb);
+                        }
+                    }
                 }
             }
         });
-        iteration++;
-    }
 
-    // report number of clusters 
-    reportClustering(vertex_list);
+        int numActive = not_finished.get_value();
+        if (numActive == 0)
+            break;
+        iteration++;
+        if (husky::Context::get_global_tid() == 0) {
+            husky::LOG_I << "finish iteration " << iteration << std::endl;
+        }
+    }
 }
 
 template<typename ObjT>
@@ -112,7 +95,7 @@ vector<pair<int, int>> KNNConnectedComponents::getClusteringDesc(
 
     vector<pair<int, int>> agg =
         keyValueCombine<ObjT, int, int, husky::SumCombiner<int>>(
-            vertex_list,
+            obj_list,
             [&getLabel](const ObjT& v){ return getLabel(v);},
             [](const ObjT& v){ return 1;});
 
@@ -126,22 +109,18 @@ vector<pair<int, int>> KNNConnectedComponents::getClusteringDesc(
     return agg;
 }
 
-template<typename ObjT>
 void KNNConnectedComponents::reportClustering(
-    husky::ObjList<ObjT>& vertex_list) {
+    const vector<pair<int, int>>& blockToSize) {
 
     // report number of clusters 
-    auto agg = KNNConnectedComponents::getClusteringDesc(vertex_list);
     if (husky::Context::get_global_tid() == 0) {
-        husky::LOG_I << "number of clusters is " << agg.size() << std::endl;
+        husky::LOG_I << "number of clusters is " << blockToSize.size() << std::endl;
         husky::LOG_I << "present cluster_id and size in (cluster_id, size)" << std::endl;
-        for (int i = 0; i < agg.size(); ++i) {
-            husky::LOG_I << "(" << agg[i].first << ", " << agg[i].second << ")" << std::endl;
+        for (int i = 0; i < blockToSize.size(); ++i) {
+            husky::LOG_I << "(" << blockToSize[i].first << ", " << blockToSize[i].second << ")" << std::endl;
         }
     }
 }
 
-void knnconnected_components{
-}
 }
 }
